d09d3e406e583b15d65ff4ce7633730ec5555da1
Lucas Villalba <lucas.a.villalba@gmail.com>
2020-09-20 11:29:40 -0300

    Unmarshal

--- main.go
+++ main.go
@@ -27,6 +27,16 @@ func main() {
 		log.Panic(err)
 	}
 
-	fmt.Println(string(bs))
+	fmt.Println("Print JSON", string(bs))
+
+	xp2 := []person{}
+
+	err = json.Unmarshal(bs, &xp2)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("back into a Go data structure", xp2)
 
 }
